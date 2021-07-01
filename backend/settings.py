6dc91128ef1852a64502db59b98dbf1a9cf5771c
DarshSikka <>
2021-07-01 06:03:05 +0000

    First commit from repl

--- backend/settings.py
+++ backend/settings.py
@@ -14,14 +14,15 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 CORS_ORIGIN_ALLOW_ALL = True
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'x02avh039wmxu^^^=-smli+*wd)mv%=@9d!m&fl@+qo76!qqe!'
-
+SECRET_KEY ='x02avh039wmxu^^^=-smli+*wd)mv%=@9d!m&fl@+qo76!qqe!'
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
@@ -36,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'maininfo',
-    'corsheaders'
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
--- backend/urls.py
+++ backend/urls.py
@@ -15,8 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("world/", include("maininfo.urls"))
+    path("api/", include("maininfo.urls"))
 ]
